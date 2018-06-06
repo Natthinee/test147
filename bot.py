c2c99e34dc265ff54341f7a256c4d811440ccbdf
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-06 21:17:58 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def movie(event):
-    userr = mongo.khim.users
+    userr = mongo.khim.user
     if event.message.text == 'สวัสดี':
         question = event.message.text
         answer = 'สวัสดีจ้า'        
