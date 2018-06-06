cf0705e59c03dad437ebeb08db6735d3b36571c1
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-06 16:24:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,16 +6,14 @@ from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 from flask.ext.pymongo import PyMongo
 
-from sss import findmovie
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(
-    'IzXs2WdxBaxjM/BTdVQ43pEYgt1O8BRRrEAOztjHPMfRUmM0BYtD4VRZg7MLMSyi1mWqI3vdPl08HfmsCUiBM1QJKc0OF89EfbEPIHEG+pKHO85//3Zvo+Qcf9MDZoFwe2m+cjasnyvwYZ3xPQNWPgdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('IzXs2WdxBaxjM/BTdVQ43pEYgt1O8BRRrEAOztjHPMfRUmM0BYtD4VRZg7MLMSyi1mWqI3vdPl08HfmsCUiBM1QJKc0OF89EfbEPIHEG+pKHO85//3Zvo+Qcf9MDZoFwe2m+cjasnyvwYZ3xPQNWPgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('0dc428295a377a2e3ee1bda97af613e2')
-app.config['MONGO_DBNAME'] = 'khim'
-app.config['MONGO_URI'] = 'mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim'
-mongo = PyMongo(app)
+#app.config['MONGO_DBNAME'] = 'khim'
+#app.config['MONGO_URI'] = 'mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim'
+#mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
@@ -44,16 +42,14 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def movie(event):
-    user = mongo.db.khim
+   # user = mongo.db.khim
     if event.message.text == 'สวัสดี':
         question =event.message.text
-        answer = 'สวัสดีจ้า'
-        q  =findmovie()
-        
+        answer = 'สวัสดีจ้า'        
         
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
 
-        user.insert({"Question": question, "Answer": answer})
+        #user.insert({"Question": question, "Answer": answer})
 
 
 
