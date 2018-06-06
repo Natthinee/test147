f359389198a552220dceeb25dda9a6ae5574758e
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-07 01:37:04 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -116,7 +116,7 @@ def webhook():
 @handler.add(MessageEvent, message=TextMessage)
 def movie(event):
     userr = mongo.db.user
-    if event.message.text in evaluation_form['eval']['greet']:
+    if event.message.text == evaluation_form['eval']['greet']:
         question = event.message.text
         answer = 'สวัสดี'       
         userr.insert({"Question": question, "Answer": answer})
