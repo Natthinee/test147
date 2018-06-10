380b06459e5a8c67e091e97a59c884a7a23415d3
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-11 00:34:16 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ tt = []
 def find1(question,answer):
    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
    db  = client.khim.user
-   data = db.user.find('สวัสดี').sort('ขิมเองจ้า')
+   data = db.user.find()
    tt.append(data)
    return tt
 
