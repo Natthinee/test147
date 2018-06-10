665cef6ec6b505461adc8cddf848531fb4989d87
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-11 00:22:40 +0700

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def find1(question,answer):
    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
    db  = client.khim.user
    #data = db.user.find()
-   ttt = posts.find("_id": {"$oid": "5b1cd22a2a76fe1393ef95f5"}).sort("Question")
+   ttt = db.user.find("_id": {"$oid": "5b1cd22a2a76fe1393ef95f5"}).sort("Question")
    for post in ttt:
       tt.append(post)
    return tt
