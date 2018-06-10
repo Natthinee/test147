2b262bb582f27eb9cf3e6a5990bbfa05d851afa7
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-10 23:50:02 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ tt = []
 def find1(question,answer):
    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
    db  = client.khim.user
-   w = db.user.find_one()
+   #w = db.user.find_one()
    #w = db.find({'Question': 'แบบประเมิน' }).sort(answer)
-   return w
+   for item in db.user.find():
+   return item
