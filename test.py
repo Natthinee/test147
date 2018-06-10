a78e7d606003ee69f8a12ede03c73d26a8cd14a0
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-10 22:46:19 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,15 @@ tt = []
 def find1(question,answer):
    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
    db  = client.khim.user
-   w = db.find({'Question': question }).sort('Answer')
-   for i in w:
-      tt.append(i)
-   return tt
+   w = db.find({'Question': question }).sort(answer)
+   array = []
+    for i in cursor:
+        a = i
+        for key, value in a.items():
+            if key == 'Answer':
+                array.append(value)
+    if array!=[]:
+       return array[-1]
+    else:
+        return ''
+   
