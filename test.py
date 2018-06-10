191e2ff2a5c73bac41f8f9c6c995c93d68a71768
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-11 00:51:27 +0700

    Update test.py

--- test.py
+++ test.py
@@ -7,9 +7,7 @@ def find1(question,answer):
    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
    db  = client.khim.user
    #data = db.user.find()
-   data = db.system.indexes.find(  "_id": {
-        "$oid": "5b1d0aaf31658a00090aa563"
-    })
+   data = db.system.indexes.find()
    tt.append(data)
    return tt
 
