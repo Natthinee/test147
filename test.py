b396012c5c1ede3961da4b5d6f448ea5ec9276c2
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-11 01:10:11 +0700

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def find1(question,answer):
    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
    db  = client.khim.user
    #data = db.user.find()
-   data = db.user.find(Question).sort("เเบบประเมิน")
+   data = db.user.find("Question": "เเบบประเมิน").sort("Answer": "รู้สึกหลับยาก หรือหลับๆ ตื่นๆ หรือหลับมากไป บ้างหรือเปล่าอ่ะ ?? Zzzz")
    for i in data:
         tt.append(i) 
    return tt
