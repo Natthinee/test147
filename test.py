f816081453f121db5ca57c1aca9de333c72f0d0c
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-10 17:34:49 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+import pymongo
+from pymongo import MongoClient
+tt = []
+
+def find1():
+   client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
+   db  = client.khim.user
+   g = db.insert({'สวัสดี':'ขิมเองจ้า'})
+   w = db.find({'Question':  "Dbfusv"}).sort("Answer")
+   for i in w:
+      tt.append(i)
+   return tt
