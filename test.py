a28463c2673300fde2619908548dcb588846b91f
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-11 00:05:30 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ tt = []
 def find1(question,answer):
    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
    db  = client.khim.user
-   data = db.find()
+   data = db.user.find()
    for i in data:
       tt.append(i)
    return tt
