7e9332a373e6b08e6cb215392db5c511f2c5bc8f
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-11 23:16:55 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import pymongo
 from pymongo import MongoClient
 import json
-number = ['0', '1', '2', '3']
 quest9 = open("Quest9.txt", "r", encoding='utf-8-sig')
 quest9 = quest9.read().split(',')
 tt = []
 score = 0
-def find1():
+def find1(face,question1):
      client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
      db  = client.khim.user
      for i in db.find():
-          if i['Answer'] in number:
-               tt.append(i['Answer'])
-     return len(tt)
+          if question1 not in i['Answer']:
+                return face + question1
+          else:
+               return 'ทดสอบใหม่'
 
   
