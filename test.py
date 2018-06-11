8e2a20f315853c2937a9f56fe3894d0c17ad7f67
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-11 21:18:31 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import pymongo
 from pymongo import MongoClient
 import json
+number = ['0', '1', '2', '3']
+quest9 = open("Quest9.txt", "r", encoding='utf-8-sig')
+quest9 = quest9.read().split(',')
 tt = []
+score = 0
 def find1():
      client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
      db  = client.khim.user
      for i in db.find():
-          if i["Question"] ==
-     return tt
+          if i["Answer"] == quest9:
+               score = score + 1
+     return score
 
   
