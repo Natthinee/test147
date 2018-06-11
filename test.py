9a5b222e5d09e9a85486c798c5876831bfaf8614
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-11 21:00:55 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,10 @@ from pymongo import MongoClient
 import json
 tt = []
 def find1():
-     #client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
-     #db  = client.khim.user
-     #g = db.insert({'สวัสดี':'12','จ้า':'n'})
-#g2 = db.user.insert({'สวัสดี':'21'})
-#db.users.remove({z:'abc'});
-     #for i in db.find():
-          #tt.append(i['สวัสดีจ้า'])
+     client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
+     db  = client.khim.user
+     for i in db.find():
+          tt.append(i["Question"])
      return tt
 
   
