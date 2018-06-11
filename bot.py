84aeec591b21ab481e064adf2585f82f20061766
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-12 06:56:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -154,13 +154,18 @@ def godaun(event):
         answer = random.choice(evaluation_form['eval']['answer'] )
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
     elif question in evaluation_form['eval']['ques']:
-        question1 = QC1[1]
+        question1 = QC1[0]
         face = random.choice(evaluation_form['eval']['wordap'])
         answer =face+question1+'\n'+ setscoreq9['score']['pprint']+'\n'+ please['ple']['ple'] 
         userr.insert({"UserID":userid,"Question": question, "Answer": question1})
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
     elif question in number:
-        question1 ='ทดสอบ'
+        question1 = QC1[1]
+        face = random.choice(evaluation_form['eval']['wordap'])
+        answer = face + question1 +'\n'+ setscoreq9['score']['pprint']+'\n'+ please['ple']['ple']
+        userr.insert({"UserID":userid,"Question": question, "Answer": question1})
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
+        question1 = QC1[2]
         face = random.choice(evaluation_form['eval']['wordap'])
         answer = face + question1 +'\n'+ setscoreq9['score']['pprint']+'\n'+ please['ple']['ple']
         userr.insert({"UserID":userid,"Question": question, "Answer": question1})
