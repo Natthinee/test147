53ef39ac12133978c7cd7ebf378c6cf5d76cb604
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-11 21:38:22 +0700

    Update test.py

--- test.py
+++ test.py
@@ -10,8 +10,8 @@ def find1():
      client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
      db  = client.khim.user
      for i in db.find():
-          if i["Answer"] == quest9:
-               score = score + 1
+          if i['Answer'] in number:
+               score = score + int(i['Answer'])
      return score
 
   
