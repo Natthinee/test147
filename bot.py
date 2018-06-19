efff0207972089d1d8658cbe0c30275579582b85
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-19 22:48:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -288,15 +288,8 @@ def godaun(event):
                                 label='ออกกำลังกายคลายเศร้า',
                                 uri='http://www.bangkokhealth.com/health/article/%E0%B8%AD%E0%B8%AD%E0%B8%81%E0%B8%81%E0%B8%B3%E0%B8%A5%E0%B8%B1%E0%B8%87%E0%B8%81%E0%B8%B2%E0%B8%A2%E0%B8%84%E0%B8%A5%E0%B8%B2%E0%B8%A2%E0%B9%80%E0%B8%A8%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%9A%E0%B8%A3%E0%B8%A3%E0%B9%80%E0%B8%97%E0%B8%B2%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%94-868'
                             
-                            ),
-                            URITemplateAction(
-                                label='10วิธีดึงตัวเองจากภาวะเศร้า',
-                                uri='https://nextempire.co/stories/thought-leader/10-%E0%B8%A7%E0%B8%B4%E0%B8%98%E0%B8%B5-%E0%B8%94%E0%B8%B6%E0%B8%87%E0%B8%95%E0%B8%B1%E0%B8%A7%E0%B9%80%E0%B8%AD%E0%B8%87%E0%B8%88%E0%B8%B2%E0%B8%81%E0%B8%A0%E0%B8%B2%E0%B8%A7%E0%B8%B0%E0%B9%80%E0%B8%A8%E0%B8%A3%E0%B9%89%E0%B8%B2-%E0%B8%A3%E0%B8%B5%E0%B8%AA%E0%B8%95%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%97%E0%B8%95%E0%B8%B1%E0%B8%A7%E0%B9%80%E0%B8%A3%E0%B8%B2%E0%B9%80%E0%B8%9E%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B9%80%E0%B8%94%E0%B8%B4%E0%B8%99%E0%B8%95%E0%B9%88%E0%B8%AD%E0%B9%84%E0%B8%9B/901#jilsphkl37'
-                            ),
-                            URITemplateAction(
-                                label='4วิธีง่ายๆปรับสภาพจิตใจ',
-                                uri='https://www.hongthongrice.com/life/5986/4feelgood/'
                             )
+                           
                         ]
                     )
                  ]
