26d23d7b262e1c538adf4a08c6548a07484af508
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-24 09:28:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -103,6 +103,9 @@ def godaun(event):
     if question in evaluation_form['eval']['greet']:
         answer = random.choice(evaluation_form['eval']['answer'] )
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
+    if question in evaluation_form['eval']['greet']:
+        answer = random.choice(evaluation_form['eval']['answer'] )
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
     elif question in evaluation_form['eval']['ques']:
         question1 = str(find1(userid,question)) ######test#####
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=question1))
