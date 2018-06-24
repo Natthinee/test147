e9e4e83a627d96e97c339b23de5bcb8295c49d93
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-24 18:01:42 +0700

    Update province1.py

--- province1.py
+++ province1.py
@@ -7,18 +7,20 @@ Created on Sun Jun 24 09:17:34 2018
 
 import pymongo
 from pymongo import MongoClient
+import decimal 
+d = decimal.Decimal 
 
 client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
 db  = client.khim.province_Hospital
 def Latitudee(question):
     for i in db.find():
         if(i['province'] == question):
-            return float(i['Latitude'])
+            return d(i['Latitude'])
                 
 def longtitutee(question):
     for i in db.find():
         if(i['province'] == question):
-            return float(i['longitude'])
+            return d(i['longitude'])
         
 def hospitalName(question):
     for i in db.find():
