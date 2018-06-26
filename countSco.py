9ca8b849f770b86f924c3552fa9594c059038a20
khim ki khim <CHANLONN_N@hotmail.com>
2018-06-27 05:06:02 +0700

    Update countSco.py

--- countSco.py
+++ countSco.py
@@ -6,9 +6,12 @@ Created on Wed Jun 27 04:41:51 2018
 """
 import pymongo
 from pymongo import MongoClient
-client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
-db  = client.khim.user
+
+
 def scoreC():
+    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
+    db  = client.khim.user
+    count = 0
     for i in db.find():
         if i['Question'] == '1':
             score = i['Question']
