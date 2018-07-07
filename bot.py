fa48b89381837f82d1863374ec0fd2c06689c917
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-07 17:34:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ Created on Wed Jun  6 21:31:06 2018
 """
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
+from linebot import LineBotApi
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
                             SourceUser, SourceGroup, SourceRoom,
