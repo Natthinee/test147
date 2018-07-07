80cad0d8a22b5cfdde05c810ffe4333e5550b30c
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-07 20:00:52 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -125,7 +125,7 @@ def godaun(event):
         #line_bot_api.push_message(userid, location_message)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
         #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=location_message))
-      elif question == 'test':
+    elif question == 'test':
         buttons_template_message = TemplateSendMessage(
             alt_text='Buttons template',
             template=ButtonsTemplate(
