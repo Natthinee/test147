7ae8526cd23ef724832a8176dcb89be9827da99e
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-09 22:16:36 +0700

    Update test.py

--- test.py
+++ test.py
@@ -84,8 +84,8 @@ def find1(userid,question):
     db.insert({"UserID":userid,"Round":count1,"No":arr,"Question": question, "Answer": answer})
     else:
        for i in db.find():
-          if userid == i[UserID]
-             count1 = 1
+          if userid == i[UserID]:
+             count1 = count1+1
              arr = arr + 1
              answer = t9[1]
     db.insert({"UserID":userid,"Round":count1,"No":arr,"Question": question, "Answer": answer}) 
