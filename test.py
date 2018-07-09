1e096bf347b81de9a1c391d17ff01d6d51bc7f89
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-09 20:41:36 +0700

    Update test.py

--- test.py
+++ test.py
@@ -75,9 +75,7 @@ ple8 = 'ตอบว่า "ใช่" หากเคยมีเหตุก�
 def find1(userid,question):
      client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
      db  = client.khim.user
-     for i in db.find()
-         if userid == i['UserID']:
-              count1 = count1 + 1
+     
      return count1
   
   
