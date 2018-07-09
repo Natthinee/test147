337474021b014edbfd3fbd7946bd32fe55aac2e2
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-09 21:18:56 +0700

    Update test.py

--- test.py
+++ test.py
@@ -75,7 +75,11 @@ ple8 = 'ตอบว่า "ใช่" หากเคยมีเหตุก�
 def find1(userid,question):
      client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
      db  = client.khim.user
-     count1  = count1 + 1
+     for i in db.find(UserID):
+          if userid==i['UserID']
+              count1  = count1 + 1
+              answer = 'test'
+              db.insert({"UserID":userid,"Round":count1,"No":No,"Question": question, "Answer": answer})
      return count1 
 def find2(userid,question):
     #db.insert({"UserID":userid,"Round":count1,"No":No,"Question": question, "Answer": answer})
