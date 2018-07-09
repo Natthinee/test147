e68091185a132fc29c6ff2974d9d62dcf0308081
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-09 13:54:49 +0700

    Update test.py

--- test.py
+++ test.py
@@ -108,25 +108,19 @@ def find3(userid,question):
               answer = quest8[1]
           if(countData == 13):
               answer = quest8[2]
-              if(question == 'มี'):
-                  count1 = 3
-              else:
-                  count1 = 4   
-          if(count1 == 3):
-              if(countData == 14):
-                  answer = quest8[3]  
-              if(countData == 15):
-                  answer = quest8[4]
-             if(countData == 16):
-                  answer = quest8[5]
-              if(countData == 17):
-                  answer = quest8[6]
-              if(countData == 18):
-                  answer = quest8[7]
-              if(countData == 19):
-                  answer = quest8[8]
+          if(countData == 14):
+              answer = quest8[4]  
+          if(countData == 15):
+              answer = quest8[5]
+          if(countData == 16):
+              answer = quest8[6]
+          if(countData == 17):
+              answer = quest8[7]
+          if(countData == 18):
+              answer = quest8[8]
+             
   
-                  
+               
           db.insert({"UserID":userid,"Question": question, "Answer": answer})
           return answer
         
