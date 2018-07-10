8a38b9355513dfa1befa05faa490bd8ae40b9aff
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-10 12:03:44 +0700

    Update countSco.py

--- countSco.py
+++ countSco.py
@@ -8,13 +8,14 @@ import pymongo
 from pymongo import MongoClient
 number = ['0','1','2','3']
 
-def scoreC():
+def scoreC(userid,question):
     client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
     db  = client.khim.user
     count = 0
     for i in db.find():
-        if i['Question'] in number:
+        if(userid == i['UserID']):
             score = i['Question']
+        if i['Question'] in number:
             count = int(score) + count
     return count
         
