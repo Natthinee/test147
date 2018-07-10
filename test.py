f17e5e8dcba013e4796e44761fed980844532238
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-10 19:45:02 +0700

    Update test.py

--- test.py
+++ test.py
@@ -292,6 +292,15 @@ def findxy(userid,question):
         No1 = No
         db.insert({"UserID":userid,"Round":count2,"No":No1,"Question": question, "Answer": answer})
         return answer 
+      
+def deleteQu(userid,question):
+   client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
+   db  = client.khim.user
+   dd  = client.khim.find2
+   db.delete_many({'UserID':userid})
+   dd.delete_many({'UserID':userid})
+   return 'มาทำเเบบประเมินกันเถอะ'
+  
 
 
         
