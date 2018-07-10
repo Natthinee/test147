d51c95efd44d5b6d82aa7c91a8f286e9ad29fa36
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-10 13:58:31 +0700

    Update countSco.py

--- countSco.py
+++ countSco.py
@@ -21,15 +21,4 @@ def scoreC(userid,question):
     print(count)
     return str(count)
 
-def countQues(userid,question):
-    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
-    db  = client.khim.user
-    count = 0
-    score = 0
-    for i in db.find():
-        if(userid == i['UserID']):
-            if i['Question'] in number:
-                count = count+1
-    print(count)            
-    return str(count)
         
