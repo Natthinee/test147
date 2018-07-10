ef3fa0e9504e20af419f90a6e805c52ffc84da72
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-10 17:53:01 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -205,26 +205,28 @@ def godaun(event):
         #print(confirm_template_message)
         
         line_bot_api.reply_message(event.reply_token, confirm_template_message)
-    elif question in ans2:
-        answer = str(find2(userid,question))
-        if(question in ans2):
-            confirm_template_message = TemplateSendMessage(
-                alt_text='Confirm template',
-                template=ConfirmTemplate(
-                    text=answer,
-                    actions=[
-                        MessageTemplateAction(
-                            label='มี',
-                            text='มี'
-                        ),
-                        MessageTemplateAction(
-                            label='ไม่มี',
-                            text='ไม่มี'
-                        )
-                    ]
-                )
-            ) 
-        line_bot_api.reply_message(event.reply_token, confirm_template_message) 
+    elif question  in ans2:
+        confirm_template_message = TemplateSendMessage(
+            alt_text='Confirm template',
+            template=ConfirmTemplate(
+               text=str(find2(userid,question)),
+               actions=[
+                    MessageTemplateAction(
+                        label='มี',
+                        text='มี'
+                    ),
+                    MessageTemplateAction(
+                        label='ไม่มี',
+                        text='ไม่มี'
+                    )
+               ]
+            )
+        )
+        #print("confirm_template_message")        
+        #print(confirm_template_message)
+        
+        line_bot_api.reply_message(event.reply_token, confirm_template_message)
+   
     elif question in 'ทำไรได้บ้าง':
         #question1 = str(find1(userid,question))
         #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=question1))
