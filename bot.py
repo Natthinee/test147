daf2aa78454a92ffc245295474679931f2b82a21
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-10 15:49:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ from pymongo import MongoClient
 import json
 import random
 from province1 import Latitudee,longtitutee,hospitalName,provincee
-from countSco import scoreC
+from countSco import scoreC,scoreQ2
 from test import find1,find2,find3,findx,findy,findxy,findxx
 question1 = ''
 evaluation_form = {}
