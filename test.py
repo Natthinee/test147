1afd1a01eb9a7adbdf6ab51fa03901dc82dee2c7
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-22 22:44:11 +0700

    Update test.py

--- test.py
+++ test.py
@@ -92,7 +92,7 @@ game = 'พักสมองสักเเปบดีกว่าน้าา
 def find1(userid,question):
    client = MongoClient('mongodb://khimmy:Kk2047849@ds147030.mlab.com:47030/khim')
    db  = client.khim.user
-   #numb = ['0','1','2','3']
+   numb = ['0','1','2','3']
    count1 = 0 
    count2 = 0
    No = 0
@@ -100,13 +100,15 @@ def find1(userid,question):
    round = 1
    for i in db.find():
         if(userid==i['UserID']):
-            #if i['Question'] in numb:
-            count1 = count1+1
-            No =  No+1
-            print(count1)
-            print(No)
+            if i['Question'] in numb:
+                  count1 = count1+1
+                  No =  No+1
+                  print(count1)
+                  print(No)
             if i['Question'] == "ทำต่อจากเดิม":
-                   count1 = count1-2            
+                  count1 = count1-1
+                  No =  No-1
+                  print(count1)
    for i in db.find():
         #print(i['UserID'])
         idsub.append(i['UserID'])
