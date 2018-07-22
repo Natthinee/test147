aabc2a6fbc6501ad5e450fa59d4692a571bed7ff
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-23 01:13:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -277,6 +277,7 @@ def godaun(event):
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
         
     elif question == "ทำต่อจากเดิม":
+        db.delete_many({'No':findxx(userid,question)})
         answer = str(find1(userid,question))
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
                 
