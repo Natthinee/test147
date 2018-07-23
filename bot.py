01c712130ca74253e1575398aba04c4bf05f7afa
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-23 10:46:13 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -483,7 +483,7 @@ def godaun(event):
              answer = random.choice(evaluation_form['eval']['answer']) + ' เอ๊ะๆ ดูเหมือนยังทำเเบบประเมินไม่เสร็จเลย ถ้าอยากทำต่ออันเดิม พิมพ์คำว่า "ทำต่ออันเดิม" เเต่ถ้าอยากเริ่มใหม่ ให้พิมพ์คำว่า "เริ่มใหม่"?'
              sticker_message = StickerSendMessage(
              package_id='2',
-             sticker_id='24')
+             sticker_id='22')
              line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
              line_bot_api.push_message(userid, sticker_message)
              #answer = random.choice(evaluation_form['eval']['answer']) + ' เอ๊ะๆ วันนี้รู้สึกยังไงบ้างเอ่ย?'
