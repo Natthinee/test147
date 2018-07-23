9b229ee0e226553774ad32010182b74d4edb1028
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-23 10:51:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -479,7 +479,7 @@ def godaun(event):
              sticker_id='22')
              line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
              line_bot_api.push_message(userid, sticker_message)
-       else:
+        if Qy>"0":
              answer = random.choice(evaluation_form['eval']['answer']) + ' เอ๊ะๆ ดูเหมือนยังทำเเบบประเมินไม่เสร็จเลย ถ้าอยากทำต่ออันเดิม พิมพ์คำว่า "ทำต่ออันเดิม" เเต่ถ้าอยากเริ่มใหม่ ให้พิมพ์คำว่า "เริ่มใหม่"?'
              sticker_message = StickerSendMessage(
              package_id='2',
