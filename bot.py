1adb659a27190bb040a746dcb9dc4c7a4784d67e
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-23 12:00:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1095,7 +1095,7 @@ def godaun(event):
         buttons_template_message = TemplateSendMessage(
             alt_text='Buttons template',
             template=ButtonsTemplate(
-                thumbnail_image_url='https://i.imgur.com/gaHNN7o.jpg',
+                thumbnail_image_url='https://i.imgur.com/AXPAHUU.jpg',
                 title='นาฬิกาเรือนไหนของจริง',
                 text='เลือกสิๆ',
                 actions=[
