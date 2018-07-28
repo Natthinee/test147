fcc0deb37484b5c9d40a7ccb1602e575ff47cafc
khim ki khim <CHANLONN_N@hotmail.com>
2018-07-28 17:56:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1202,8 +1202,8 @@ def godaun(event):
           )
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
         
-     elif question == "ดูโรงพยาบาลต่อ":
-         buttons_template_message = TemplateSendMessage(
+    elif question == "ดูโรงพยาบาลต่อ":
+        buttons_template_message = TemplateSendMessage(
             alt_text='Buttons template',
             template=ButtonsTemplate(
                 title = 'สะดวกไหนเลือกเลยนะจ๊ะ',
