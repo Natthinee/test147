20b6705a902fe40109db484e2ea48773c3e032ec
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-05 20:18:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1606,7 +1606,7 @@ def handle_content_message(event):
     #line_bot_api.reply_message(event.reply_token, [TextSendMessage(text='Save content.'),
             #TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
         #])
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="test"))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="ooooo")
     
  
 
