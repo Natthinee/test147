66cea33ab10a9ff8a8fd92e30dd20fa7d2a9e414
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-07 05:51:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1602,7 +1602,7 @@ def handle_content_message(event):
         file_path = file  + '.' + ext
     dist_name = os.path.basename(file_path)
     os.rename(file, file_path)
-    s3.upload_file(  file_path, BUCKET_NAME, ile_path)
+    s3.upload_file( file_path, BUCKET_NAME, file_path)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text="ooooo"))
     
     #dist_name = os.path.basename(dist_path)
