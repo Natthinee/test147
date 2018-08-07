2e64c041375e87437c04529366fc3f86d6a5b604
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-07 14:24:44 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1594,7 +1594,7 @@ def handle_content_message(event):
     print(event.message.id)
     print("-------------------------")
     print(ext)
-    with open("filename", "wb) as tt:
+    with open("filename", "wb") as tt:
         for chunk in message_content.iter_content():
             print(chunk)
             tt.write(chunk)
