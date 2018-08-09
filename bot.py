e67149cd2595523c60ebec8cbce6f503130edf0e
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-10 03:31:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1637,7 +1637,7 @@ def handle_content_message(event):
            #print(chunk)
            tt.write(chunk)
            file = tt.name
-       file_path = file  + '.' + ext
+       file_path = file  + '.' + 'wav'
    dist_name = os.path.basename(file_path)
    os.stat(file)
    os.rename(file, file_path)
