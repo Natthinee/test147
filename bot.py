e988103f8ffd934fd835a7a232fa73681dd85a2d
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-10 04:21:07 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1659,7 +1659,7 @@ def handle_content_message(event):
    #client.upload_file(Bucket=BUCKET_NAME, Key='test.wav', Filename=file_path, Config=TransferConfig(use_threads=False))
    #s3.Bucket(BUCKET_NAME).put_object(Key='test.wav', Body=data)
    #line_bot_api.reply_message(event.reply_token, TextSendMessage(text="ooooo"))
-   line_bot_api.reply_message(event.reply_token, [TextSendMessage(text=str(speechcheck(filename))),TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))])
+   line_bot_api.reply_message(event.reply_token, [TextSendMessage(text=str(speechcheck(dist_name))),TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))])
     
     #dist_name = os.path.basename(dist_path)
     #os.rename(tempfile_path, dist_path)
