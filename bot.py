84900777a8093ca4d9f7b75d92a906f74ecdf911
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-10 03:10:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1644,7 +1644,7 @@ def handle_content_message(event):
    #sign_s3()
    print(".......................")
    print(".....................up")
-   #client = boto3.client('s3')
+   client = boto3.client('s3')
    print(".....................upload")
    print(dist_name)
    print(file_path)
