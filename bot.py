2921403c636bb9a21b96cdbc35d5235290781fbd
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-10 06:50:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1657,10 +1657,11 @@ def handle_content_message(event):
    #client.upload_file(Bucket=BUCKET_NAME, Key='test.wav', Filename=file_path)
    #client.upload_file(file_path, '/'.join([BUCKET_NAME,'k.wav']), Key= file_path)
    s3.upload_file(file_path, BUCKET_NAME,dist_name )
-   t = speechword(tt)
    url = 'https://s3-ap-southeast-1.amazonaws.com/khim/U2cd26d49ace18bd6cfce4e53160808cb23m2o3hk.wav'
    r = requests.get(url)
-   print(r)
+   t = speechword(r)
+   print(t)
+   
    #file
    #client = boto3.client("s3")
    #client.upload_file(Bucket=BUCKET_NAME, Key='test.wav', Filename=file_path, Config=TransferConfig(use_threads=False))
