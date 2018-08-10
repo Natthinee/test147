8d24bed28267f0be700976c4801a39212a07ac05
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-10 15:34:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -963,7 +963,7 @@ def godaun(event):
         #print("confirm_template_message")        
         #print(confirm_template_message)
         line_bot_api.reply_message(event.reply_token, confirm_template_message)
-    elif question == ไม่มี:
+    elif question == "ไม่มี":
         if(question in ans2):
             confirm_template_message = TemplateSendMessage(
                 alt_text='Confirm template',
