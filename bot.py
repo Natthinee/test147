c667e9b9db59e54ba32bea8ba2bfa693436812ba
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-10 11:34:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1654,13 +1654,13 @@ def handle_content_message(event):
    s3.upload_file(file_path, BUCKET_NAME,dist_name )
    #url = 'https://s3-ap-southeast-1.amazonaws.com/khim/U2cd26d49ace18bd6cfce4e53160808cb23m2o3hk.wav'
    #s3.Bucket('khim').download_file(U2cd26d49ace18bd6cfce4e53160808cb23m2o3hk.wav, "t.wav")
- 
+   answer = "ช่วงนี้มีความรู้สึกยังไงบ้างเอ่ย ??"
    #file
    #client = boto3.client("s3")
    #client.upload_file(Bucket=BUCKET_NAME, Key='test.wav', Filename=file_path, Config=TransferConfig(use_threads=False))
    #s3.Bucket(BUCKET_NAME).put_object(Key='test.wav', Body=data)
    #line_bot_api.reply_message(event.reply_token, TextSendMessage(text="ooooo"))
-   line_bot_api.reply_message(event.reply_token, [TextSendMessage(text="test"),TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))])
+   line_bot_api.reply_message(event.reply_token, [TextSendMessage(text="answer"),TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))])
    
     #dist_name = os.path.basename(dist_path)
     #os.rename(tempfile_path, dist_path)
